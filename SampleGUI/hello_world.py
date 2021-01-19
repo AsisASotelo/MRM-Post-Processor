21268b3370847a6a2f03f7bc12e8012d962d0ebd
AsisASotelo <asis.a.sotelo@gmail.com>
2021-01-19 11:35:02 -0800

    Initial Commit

---
+++ SampleGUI/hello_world.py
@@ -0,0 +1,18 @@
+## hello_world.py
+## 
+## Asis A Sotelo
+## January 19, 2021
+## PyQt5 Sample Program
+
+
+from PyQt5 import QtWidgets
+
+app = QtWidgets.QApplication([])
+
+window = QtWidgets.QWidget(windowTitle='Hello Qt')
+window.show()
+
+app.exec()
+
+
+
---
+++ SampleGUI/qt_template.py
@@ -0,0 +1,11 @@
+## qt_template.py
+##
+## Asis A Sotelo
+## January 19, 2021
+##
+## A Template for PyQt GUIs
+##
+
+
+import sys 
+from PyQt5
